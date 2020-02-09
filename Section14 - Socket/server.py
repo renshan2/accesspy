95b0b504a1efc86ddeae2193fffeb9a4a29486c7
m <office@cabinjohnconsulting.com>
2020-02-09 15:43:21 -0500

    server.py updated

--- Section14 - Socket/server.py
+++ Section14 - Socket/server.py
@@ -10,6 +10,6 @@ print('Server is up. Listening for connections...')
 client, address = s.accept()
 print('Connection to', address, 'estabilished\n')
 print('Client object:', client, '\n')
-
+#sent message to client
 client.send(bytes('Hello! Welcome to socket programming.', 'utf-8'))
 s.close()
